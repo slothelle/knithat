c967a14c0118609bb2b98f590a3d51eb47347feb
Liz Abinante <eabinante@gmail.com>
2013-06-17 21:28:30 -0500

    most helper methods for pattern generator completed; mockup of user profile page

--- app/helpers/generator.rb
+++ app/helpers/generator.rb
@@ -1,15 +1,15 @@
-ABBREVIATIONS = {k: 'knit', p: 'purl', sl: 'slip', pm: 'place marker', 
-            slm: 'slip marker', k2tog: 'knit 2 together', co: 'cast on',
-            bo: 'bind off', st: 'stitch', sts: 'stitches', rpt: 'repeat',
-            rpts: 'repeats'}
+ABBREVIATIONS = { k: 'knit', p: 'purl', sl: 'slip', pm: 'place marker', 
+                  slm: 'slip marker', k2tog: 'knit 2 together', co: 'cast on',
+                  bo: 'bind off', st: 'stitch', sts: 'stitches', rpt: 'repeat',
+                  rpts: 'repeats' }
 
 SIZES = { infant: [12, 16.5, 18.5], 
           child: [20, 21], 
-          adult: [22, 23, 25]}
+          adult: [22, 23, 25] }
 
 def print_abbreviations
   legend = ABBREVIATIONS.map do |abbrev, defi|
-    "<li>#{abbrev}: #{defi}</li>"
+    "<li class='abbreviations'>#{abbrev}: #{defi}</li>"
   end
   legend.join
 end 
@@ -18,9 +18,10 @@ def generate_hat_pattern(pattern_id)
   pattern = Pattern.find(pattern_id)
   get_title(pattern)
   get_user(pattern)
-  # get_yarn_info(pattern.yarn_id)
+  select_needles(pattern.needle_id)
   get_gauge(pattern)
-  # select_needles(pattern.needle_id)
+  get_yarn_info(pattern.yarn_id)
+  estimated_yardage
   cast_on_ribbing(pattern.hat_circumference, pattern.gauge_per_inch)
   ribbing_rows(pattern.hat_circumference, pattern.gauge_row_inch)
   body_height(pattern.hat_circumference, pattern.gauge_row_inch)
@@ -35,34 +36,41 @@ def get_user(pattern_obj)
   @user_id = pattern_obj.user_id
 end 
 
-# def get_yarn_info(yarn_id)
-#   yarn = Yarn.find(yarn_id)
-#   @yarn_brand = yarn.brand
-#   @yarn_color = yarn.color
-#   @yarn_weight = yarn.weight
-#   @yarn_yards = yarn.yards
-# end 
-
-def get_gauge(pattern_obj)
-  @row_gauge = pattern_obj.gauge_row_inch
-  @per_inch = pattern_obj.gauge_per_inch
+def select_needles(needle_id)
+  @larger_needle = Needle.find(needle_id).size
+  @smaller_needle = Needle.find(needle_id-1).size
 end 
 
-# def select_needles(needle_id)
-#   @large_needle = Needle.find(needle_id).size
-#   @smaller_needle = Needle.find(needle_id-1).size
-# end 
+def get_gauge(pattern_obj)
+  @row_4 = pattern_obj.gauge_row_inch * 4
+  @per_4 = pattern_obj.gauge_per_inch * 4
+end 
+
+def get_yarn_info(yarn_id)
+  yarn = Yarn.find(yarn_id)
+  @yarn_brand = yarn.brand
+  @yarn_color = yarn.color
+  @yarn_weight = yarn.yarn_weight.weight
+  @yarn_yards = yarn.yards
+  @yarn_meters = (@yarn_yards * 0.914).round(0)
+end
+
+def estimated_yardage
+  # Make magic happen
+end 
 
 def cast_on_ribbing(circumference, gauge_inch)
   raw = (circumference * gauge_inch * 0.80).round(0)
   rounded = [(raw + 8 - (raw % 8)), (raw + 9 - (raw % 9))]
   if rounded.min % 8 == 0
     @cast_on = raw 
-    @ribbing = "1x1, 2x2, or 4x4"
+    @ribbing = "1x1 or 2x2 (k1, p1 or k2, p2)"
   elsif rounded.min % 9 == 0
     @cast_on = raw
-    @ribbing = "3x3"
+    @ribbing = "3x3 (k3, p3)"
   end 
+  @slouch = (@cast_on * 1.25).round(0)
+  @beanie = (@cast_on * 0.90).round(0)
 end 
 
 def ribbing_rows(circumference, gauge_row_inch)
@@ -76,14 +84,13 @@ def ribbing_rows(circumference, gauge_row_inch)
 end 
 
 def body_height(circumference, gauge_row_inch)
-  # Write this after you check the proportions 
-  # if SIZES[:infant].include?(circumference)
-  #   @ribbing_rows = "1 inch (#{gauge_row_inch} rows)" 
-  # elsif SIZES[:child].include?(circumference)
-  #   @ribbing_rows = "1.5 inches (#{gauge_row_inch*1.5} rows)"
-  # elsif SIZES[:adult].include?(circumference)
-  #   @ribbing_rows = "2 inches (#{gauge_row_inch*2} rows)"
-  # end
+  if SIZES[:infant].include?(circumference)
+    @height = "TBD" 
+  elsif SIZES[:child].include?(circumference)
+    @height = "TBD"
+  elsif SIZES[:adult].include?(circumference)
+    @height = "6 inches (#{gauge_row_inch * 6} rows)"
+  end
 end 
 
 def crown_decreases
--- app/helpers/user.rb
+++ app/helpers/user.rb
@@ -7,3 +7,12 @@ end
 def logged_in?
   !current_user.nil?
 end
+
+
+def user_patterns(user_id)
+  patterns = Pattern.find_all_by_user_id(user_id)
+  results = patterns.map do |p|
+    "<p class='separator'>#{p.garment.category}: <a href='/pattern/#{p.garment.category.downcase}/#{p.id}'>#{p.name}</a></p>"
+  end 
+  results.join
+end 
